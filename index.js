6754f1975a53505a1cf8335889a0aca3202e1569
Rob <dreamYiZ@outlook.com>
2021-06-28 04:06:55 +0800

    +

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-export default RLog = console
+function RLog(...args) {
+
+  
+  if (args.length > 1) {
+    for (let v of args) {
+        RLog(v);
+    }
+    return RLog;
+  }
+
+  console.log(args);
+
+  return RLog;
+}
+
+export default RLog;
---
+++ test.js
@@ -0,0 +1,15 @@
+import RLog from './index.js';
+
+
+// console.log(RLog({}));
+// RLog({})([]);
+// RLog();
+
+let obj = {
+    a: 1,   
+    b: 2    
+};
+
+let arr = [1,2,3];
+
+RLog(obj)(obj)(arr, arr, 4);
